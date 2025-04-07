df78f077009a913c6814b14bf26769dcd1c8a402
Neliall2 <nicita.silenkov@yandex.ru>
2025-04-07 14:17:54 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR_BOT_TOKEN'; // Замените на ваш токен
+const token = '7649579623:AAELkjHzOElsRW53-3IX10xCZmIdgb0yF7c'; // Замените на ваш токен
 const bot = new TelegramBot(token, {polling: true});
 
 // Обработка команды /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const webAppUrl = 'https://your-domain.com/'; // Замените на URL вашего приложения
+    const webAppUrl = 'https://neliall2.github.io/TranquilTide/'; // Замените на URL вашего приложения
     
     bot.sendMessage(chatId, 'Добро пожаловать в MindSpace! Нажмите кнопку ниже, чтобы открыть приложение:', {
         reply_markup: {
